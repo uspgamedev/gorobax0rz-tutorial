1c1caf1ba86691631378e6df1d1bdb1fe3c01f2d
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-03-24 17:27:09 -0300

    Change sprite position

--- game/main.lua
+++ game/main.lua
@@ -1,11 +1,18 @@
 
 local goroba
+local goroba_x, goroba_y
 
 function love.load ()
   goroba = love.graphics.newImage 'assets/sprites/hero_goroba_small.png'
+  goroba_x, goroba_y = 200, 200
   love.graphics.setBackgroundColor(100, 100, 100, 255)
 end
 
+function love.mousepressed (x, y, button)
+  goroba_x = x
+  goroba_y = y
+end
+
 function love.draw ()
-  love.graphics.draw(goroba, 200, 200)
+  love.graphics.draw(goroba, goroba_x, goroba_y)
 end
