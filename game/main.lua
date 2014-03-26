9bfb51756c021a3cdf10ccce6f4163354cb1416c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-03-26 16:48:27 -0300

    Added hotspot do sprite

--- game/main.lua
+++ game/main.lua
@@ -7,6 +7,8 @@ function love.load ()
     x = 200,
     y = 200
   }
+  goroba.ox = goroba.sprite:getWidth()/2
+  goroba.oy = goroba.sprite:getHeight()/2
   love.graphics.setBackgroundColor(100, 100, 100, 255)
 end
 
@@ -16,5 +18,9 @@ function love.mousepressed (x, y, button)
 end
 
 function love.draw ()
-  love.graphics.draw(goroba.sprite, goroba.x, goroba.y)
+  love.graphics.draw(
+    goroba.sprite, goroba.x, goroba.y,
+    0, 1, 1, -- rotation, horizontal scale, vertical scale
+    goroba.ox, goroba.oy
+  )
 end
