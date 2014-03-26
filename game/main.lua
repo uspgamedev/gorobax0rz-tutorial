a23d6f264d5eb762dc8d4cabc6e712f43cf6c229
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-03-26 16:40:58 -0300

    Refactorated goroba sprite into a structure

--- game/main.lua
+++ game/main.lua
@@ -1,18 +1,20 @@
 
 local goroba
-local goroba_x, goroba_y
 
 function love.load ()
-  goroba = love.graphics.newImage 'assets/sprites/hero_goroba_small.png'
-  goroba_x, goroba_y = 200, 200
+  goroba = {
+    sprite = love.graphics.newImage 'assets/sprites/hero_goroba_small.png',
+    x = 200,
+    y = 200
+  }
   love.graphics.setBackgroundColor(100, 100, 100, 255)
 end
 
 function love.mousepressed (x, y, button)
-  goroba_x = x
-  goroba_y = y
+  goroba.x = x
+  goroba.y = y
 end
 
 function love.draw ()
-  love.graphics.draw(goroba, goroba_x, goroba_y)
+  love.graphics.draw(goroba.sprite, goroba.x, goroba.y)
 end
