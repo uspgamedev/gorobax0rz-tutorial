de9618a6496717abdbd8a111a874208c649c2635
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-03-26 17:03:58 -0300

    Multiple gorobas with mouse click

--- game/main.lua
+++ game/main.lua
@@ -1,26 +1,35 @@
 
-local goroba
+local gorobas
+-- We cache the image, obviously
+local goroba_sprite
 
-function love.load ()
-  goroba = {
-    sprite = love.graphics.newImage 'assets/sprites/hero_goroba_small.png',
-    x = 200,
-    y = 200
+local function makeGoroba (x, y)
+  local goroba = {
+    sprite = goroba_sprite,
+    x = x,
+    y = y
   }
   goroba.ox = goroba.sprite:getWidth()/2
   goroba.oy = goroba.sprite:getHeight()/2
+  return goroba
+end
+
+function love.load ()
+  gorobas = {}
+  goroba_sprite = love.graphics.newImage 'assets/sprites/hero_goroba_small.png'
   love.graphics.setBackgroundColor(100, 100, 100, 255)
 end
 
 function love.mousepressed (x, y, button)
-  goroba.x = x
-  goroba.y = y
+  table.insert(gorobas, makeGoroba(x, y))
 end
 
 function love.draw ()
-  love.graphics.draw(
-    goroba.sprite, goroba.x, goroba.y,
-    0, 1, 1, -- rotation, horizontal scale, vertical scale
-    goroba.ox, goroba.oy
-  )
+  for i,goroba in ipairs(gorobas) do
+    love.graphics.draw(
+      goroba.sprite, goroba.x, goroba.y,
+      0, 1, 1, -- rotation, horizontal scale, vertical scale
+      goroba.ox, goroba.oy
+    )
+  end
 end
