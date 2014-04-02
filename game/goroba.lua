7c3fdedb817392a3c792873b3d247657b5652f76
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2014-04-02 16:46:35 -0300

    Goroba's attacks have cooldown

--- game/goroba.lua
+++ game/goroba.lua
@@ -6,16 +6,22 @@ end
 function makeGoroba (x, y)
   local goroba = makeAvatar(x, y, sprites.goroba)
   goroba.time = 5.0
+  goroba.delay = 0
   return goroba
 end
 
 function updateGorobas (to_be_removed, dt)
   for i,goroba in ipairs(world.gorobas) do
-    for j,wil in ipairs(world.wils) do
-      if dist(goroba, wil) <= 150 then
-        table.insert(to_be_removed, {group='wils',index=j})
-        table.insert(world.lasers, makeLaser(goroba.x, goroba.y, wil.x, wil.y))
-        sounds.laser:play()
+    goroba.delay = math.max(goroba.delay - dt, 0)
+    if goroba.delay <= 0 then
+      for j,wil in ipairs(world.wils) do
+        if dist(goroba, wil) <= 150 then
+          table.insert(to_be_removed, {group='wils',index=j})
+          table.insert(world.lasers, makeLaser(goroba.x, goroba.y, wil.x, wil.y))
+          sounds.laser:play()
+          goroba.delay = 0.5
+          break
+        end
       end
     end
     goroba.time = math.max(goroba.time - dt, 0)
